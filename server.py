7ce590ec282320a49daf56aaa5fe8e10c36b06c0
nitinind91 <nitinind91@gmail.com>
2026-02-28 17:46:30 +0530

    1st commit:

---
+++ server.py
@@ -0,0 +1,13 @@
+from mcp.server.fastmcp import FastMCP
+
+mcp = FastMCP("greetings")
+
+
+@mcp.tool()
+def greeting(name:str) -> str:
+    "Send a greetings"
+    return f"Hello {name}, Have a Nice day! :)"
+
+
+if __name__=="__main__":
+    mcp.run(transport="streamable-http")
