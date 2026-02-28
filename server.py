c87314051fa2dfcc0c995a01b7f4b661281f6fcc
nitinind91 <nitinind91@gmail.com>
2026-02-28 19:02:14 +0530

    2nd commit

--- server.py
+++ server.py
@@ -10,4 +10,4 @@ def greeting(name:str) -> str:
 
 
 if __name__ == "__main__":
-    mcp.run(transport="streamable-http",host="0.0.0.0",port=8000)
+    mcp.run(transport="http",host="0.0.0.0",port=8000)
